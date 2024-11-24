8954d1aba74bce5b28020dc53563844c888e9fc4
ArushNo1 <arushbodla@gmail.com>
2024-11-24 17:10:00 -0500

    shell code files

--- Math/Binary_Exponentiation.cpp
+++ Math/Binary_Exponentiation.cpp
@@ -1,10 +1,21 @@
-ll binExp(ll a, ll b) {
+long long binExp(long long a, long long b) {
   if (b == 0)
     return 1;
 
-  ll res = binExp(a, b / 2) % MOD;
+  long long res = binExp(a, b / 2) % MOD;
   if (b & 1) {
     return (a * ((res * res) % MOD)) % MOD;
   } else
     return (res * res) % MOD;
+}
+
+long long binExp(long long a, long long b, long long mod) {
+  if (b == 0)
+    return 1;
+
+  long long res = binExp(a, b / 2) % mod;
+  if (b & 1) {
+    return (a * ((res * res) % mod)) % mod;
+  } else
+    return (res * res) % mod;
 }
---
+++ Templates/constantstypedefs.cpp
@@ -0,0 +1,18 @@
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef vector<bool> vb;
+
+#define endll '\n'
+#define all(x) (x).begin(), (x).end()
+#define MOD ll(1e9+7)
+#define inf int(2e31-1)
+#define INF ll(2e63-1)
+#define EPS ld(1e-9)
+#define dbg(x) cerr << __LINE__ << ": " << #x << "=" << x << endll;
