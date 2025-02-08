b25d74430fb88b4682ff7ae73834c9695fd6e214
ArushNo1 <arushbodla@gmail.com>
2025-02-08 07:41:10 -0500

    GCD

---
+++ .JAVA/Math/GCD.java
@@ -0,0 +1,11 @@
+class GCD {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            a %= b;
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }   
+}
