203f3debd1fda18111faca9dc9d65b3ce8e4d74a
ArushNo1 <arushbodla@gmail.com>
2025-02-12 15:44:17 -0500

    Removed extra space from DSU

--- DSA/Disjoint_Set_Union.cpp
+++ DSA/Disjoint_Set_Union.cpp
@@ -1,27 +1,21 @@
 
 struct DSU {
   	vector<ll> e;
-	DSU(ll N)
-	{
+	DSU(ll N) {
 		e = vector<ll>(N, -1); 
 	}
-
   	void init(ll N) { 
 		e = vector<ll>(N, -1); 
 	}
-
   	ll get(ll x) { 
 		return e[x] < 0 ? x : e[x] = get(e[x]); 
 	}
-
   	bool sameSet(ll a, ll b) {
 		return get(a) == get(b); 
 	}
-
   	ll size(ll x) {
 		return -e[get(x)]; 
 	}
-
   	bool unite(ll x, ll y) { // union by size
   	  x = get(x), y = get(y);
   	  if (x == y)
