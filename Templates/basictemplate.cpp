ea68a7b74a869049e8647f85c71221adc1707245
Brian Tay <82918615+guineapig25@users.noreply.github.com>
2025-03-17 20:05:41 -0400

    basic codeforces template, no dbg

---
+++ Templates/basictemplate.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int numtestcases;
+    cin >> numtestcases;
+    while (numtestcases--) solve();
+}
