9fa4d2ddbe3700b96f279933d96b9664f5e4d3e3
Arush Bodla <arushbodla@gmail.com>
2025-05-30 18:23:38 -0400

    indexed set debug

--- Debugging/setdbg.h
+++ Debugging/setdbg.h
@@ -30,4 +30,16 @@ ostream& operator<< (ostream& os, const unordered_set<T>& arr){
     os << "]";
     return os;
 }
+
+ostream& operator<<(ostream& os, const indexed_set& s) {
+    os << "{";
+    bool first = true;
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (!first) os << ", ";
+        os << *it;
+        first = false;
+    }
+    os << "}";
+    return os;
+}
 #endif
